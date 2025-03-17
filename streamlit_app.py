9750080024aff172cf79e89a75a5a454f778a5be
Shubaa29 <georgiuscoc69@gmail.com>
2025-03-17 14:17:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import joblib
 
 def main():
   st.title('🎈 Dermatology Machine Learning')
-  st.write('This App Using Machine Learning!')
+  st.info('This App Using Machine Learning!')
 
 #input data by user
 erythema = st.slider('Erythema , min_value = 0, max_value = 3, value = 2)
